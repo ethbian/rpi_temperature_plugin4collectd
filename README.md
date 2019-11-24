2eec22f9a703276481d51c262ac478f48c474618
Ethbian <ethbian@protonmail.com>
2019-11-24 20:36:37 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # rpi_temperature_plugin4collectd
-A collectd plugin for reporting Raspberry Pi temperature (in Celsius or Fahrenheit)
+A collectd plugin reporting Raspberry Pi temperature.  
+Tested on RPi 3 and RPi 4 with Raspbian.
+
+## quick start
+- install collectd  
+*raspbian: apt-get install collectd*
+- clone the repository  
+*git clone https://github.com/ethbian/rpi_temperature_plugin4collectd.git*
+- create directory for the plugin  
+*mkdir /usr/local/lib/collectd*
+- copy the rpi_temperature.py file to the plugin directory  
+*cp rpi_temperature.py /usr/local/lib/collectd/*
+- update the collectd config file (raspbian: **/etc/collectd/collectd.conf**) by adding to the end of the file:
+
+```
+LoadPlugin python
+<Plugin python>
+    ModulePath "/usr/local/lib/collectd"
+    Import "rpi_temperature"
+    <Module rpi_temperature>
+#        Sensor "/sys/class/thermal/thermal_zone0/temp"
+#        Celsius true
+    </Module>
+</Plugin>
+```
+
+- restart collectd  
+*raspbian: service collectd restart*
+- check logfile for possible errors  
+*raspbian: service collectd status*  
+*raspbian: tail /var/log/syslog*
+
+- uncomment and change the **Celsius** option to **false** to report temperature in Fahrenheit
+- pull requests are more than welcome if you're fixing something
---
+++ rpi_temperature.py
@@ -0,0 +1,65 @@
+"""
+This is a collectd plugin reporting Raspbeery Pi temperature (in Celsius or Fahrenheit).
+Tested on RPi3 and RPi4 with Raspbian.
+For more details see https://github.com/ethbian/rpi_temperature_plugin4collectd
+version 0.1
+"""
+try:
+    import collectd
+except ImportError:
+    raise ImportError('\n\n cannot find Python module: ' +
+                      'collectd\n try executing: pip install collectd')
+
+SENSOR = '/sys/class/thermal/thermal_zone0/temp'
+CELSIUS = True
+
+def conf(config):
+    """
+    This method has been registered as the config callback and is used to parse options from
+    given config.
+    """
+    for node in config.children:
+        key = node.key.lower()
+        val = node.values[0]
+
+        if key == 'celsius':
+            global CELSIUS
+            CELSIUS = bool(val)
+        elif key == 'sensor':
+            global SENSOR
+            SENSOR = val
+        else:
+            collectd.info('rpi_temperature: Unknown config key "%s", ignoring.' % key)
+
+
+    if CELSIUS:
+        collectd.info('rpi_temperature: reporting temperature in Celsius')
+    else:
+        collectd.info('rpi_temperature: reporting temperature in Fahrenheit')
+
+def read_temperature():
+    """
+    This method has been registered as the read callback and will be called every polling interval
+    to dispatch metrics.
+    """
+    try:
+        with open(SENSOR, 'rb') as sensor_file:
+            temperature = sensor_file.read()
+    except Exception as exception:
+        collectd.warning('Error reading temperature: check config Sensor option: "%s"' %exception)
+        temperature = 0
+    else:
+        if CELSIUS:
+            temperature = int(int(temperature)/1000)
+        else:
+            temperature = int(int(temperature)/1000*1.8+32)
+
+    collectd.Values(plugin='rpi_temperature',
+                    type='gauge',
+                    values=[temperature]).dispatch()
+
+if __name__ != '__main__':
+    collectd.register_config(conf)
+    collectd.register_read(read_temperature)
+else:
+    raise SystemExit('Nope - it is a plugin reporting to collected.')
