1219cfab22ec6d9074f4a57534db8f5adf1497e1
Ethbian <ethbian@protonmail.com>
2019-11-26 19:43:47 +0100

    print refactored

--- rpi_temperature.py
+++ rpi_temperature.py
@@ -18,9 +18,9 @@ def conf(config):
     This method has been registered as the config callback and is used to parse options from
     given config.
     """
-    for node in config.children:
-        key = node.key.lower()
-        val = node.values[0]
+    for kv in config.children:
+        key = kv.key.lower()
+        val = kv.values[0]
 
         if key == 'celsius':
             global CELSIUS
@@ -29,8 +29,7 @@ def conf(config):
             global SENSOR
             SENSOR = val
         else:
-            collectd.info('rpi_temperature: Unknown config key "%s", ignoring.' % key)
-
+            collectd.info('rpi_temperature: Ignoring unknown config key: {}'.format(key))
 
     if CELSIUS:
         collectd.info('rpi_temperature: reporting temperature in Celsius')
@@ -46,7 +45,7 @@ def read_temperature():
         with open(SENSOR, 'rb') as sensor_file:
             temperature = sensor_file.read()
     except Exception as exception:
-        collectd.warning('Error reading temperature: check config Sensor option: "%s"' %exception)
+        collectd.warning('Error reading temperature: check config Sensor option: {}'.format(exception))
         temperature = 0
     else:
         if CELSIUS:
